a3d3aa89938907ccb3741ae1a0a4e618e0b8532a
n-Frederic <xuechaozhou001@gmail.com>
2025-03-17 19:24:22 +0800

    Function

---
+++ Moreight/src/main/java/moreight/Funtion/CreateUser.java
@@ -0,0 +1,19 @@
+package moreight.Funtion;
+
+import java.io.File;
+
+public class CreateUser {
+    public static void CreateUser(String user) {
+        File folder =  new File("../TestData/" + user);
+
+        if(!folder.exists()) {
+            if(folder.mkdirs()) {
+                System.out.println("CreateUser Successfully : " + folder.getAbsolutePath());
+            } else {
+                System.out.println("CreateUser failed.");
+            }
+        } else {
+            System.out.println("The user has already existed : " + folder.getAbsolutePath());
+        }
+    }
+}
