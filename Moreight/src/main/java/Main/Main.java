fe2a4c83816d8e1db1ebeff9b41ecbd717476e43
n-Frederic <xuechaozhou001@gmail.com>
2025-03-22 19:02:05 +0800

    Function

--- Moreight/src/main/java/Main/Main.java
+++ Moreight/src/main/java/Main/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 import Function.UserManager;
 import Operate.Operating;
+import Operate.Table;
 import Parser.Field;
 
 public class Main {
@@ -15,15 +16,15 @@ public class Main {
 
         // 建库建表的测试
 //        DatabaseManager.createDataBase("testDB");
-        DatabaseManager.useDatabase("testData");
-        Field field1 = new Field("name", "VARCHAR(8)");
-        Field field2 = new Field("age", "INT");
-        ArrayList<Field> fields = new ArrayList<>();
-        fields.add(field1);
-        fields.add(field2);
-        TableManager.CreateTable("testTable",fields);
-
-        DatabaseManager.showDatabases();
+//        DatabaseManager.useDatabase("testData");
+//        Field field1 = new Field("name", "VARCHAR(8)");
+//        Field field2 = new Field("age", "INT");
+//        ArrayList<Field> fields = new ArrayList<>();
+//        fields.add(field1);
+//        fields.add(field2);
+//        TableManager.CreateTable("testTable",fields);
+//
+//        DatabaseManager.showDatabases();
 
         // 删库显示库的测试
 //        DatabaseManager.createDataBase("ndb");
@@ -31,6 +32,19 @@ public class Main {
 //        List<String> databases = DatabaseManager.showDatabases();
 //        System.out.println("Databases: " + databases);
 //        DatabaseManager.dropDatabase("ndb",2);
+
+        // 插入记录的测试
+        DatabaseManager.useDatabase("testData");
+
+        ArrayList<String> columns = new ArrayList<>();
+        columns.add("name");
+        columns.add("age");
+
+        ArrayList<Object> values = new ArrayList<>();
+        values.add("Alice");
+        values.add(25);
+
+        Table.InsertIntoValue("testTable", columns, values);
 
     }
 
--- Moreight/src/main/java/Operate/Table.java
+++ Moreight/src/main/java/Operate/Table.java
@@ -1,12 +1,62 @@
 package Operate;
 
+import Function.DatabaseManager;
 import Parser.Field;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Table {
-    public static void InsertIntoValue(String table, ArrayList<String> columns, ArrayList<String> values) {
+    private static final String DIRECTORY = "../TestData/DatabaseManager";
 
+    public static void InsertIntoValue(String table, ArrayList<String> columns, ArrayList<Object> values) {
+        try {
+            Path dataPath = Paths.get(DIRECTORY, DatabaseManager.getCurrentDatabase(), table+"_data.json");
+            JsonArray dataArray;
+
+            if(Files.exists(dataPath)) {
+                try (FileReader reader = new FileReader(dataPath.toFile())) {
+                    Gson gson = new Gson();
+                    dataArray = gson.fromJson(reader,JsonArray.class);
+                }
+            } else {
+                dataArray = new JsonArray();
+            }
+
+            JsonObject newRow = new JsonObject();
+            for (int i = 0; i < columns.size(); i++) {
+                String column = columns.get(i);
+                Object value = values.get(i);
+
+                switch (value) {
+                    case String s -> newRow.addProperty(column, s);
+                    case Integer integer -> newRow.addProperty(column, integer);
+                    case Double v -> newRow.addProperty(column, v);
+                    case Boolean b -> newRow.addProperty(column, b);
+                    case null, default ->
+                            throw new IllegalArgumentException("Unsupported value type: " + value.getClass());
+                }
+            }
+
+            dataArray.add(newRow);
+
+            try (FileWriter writer = new FileWriter(dataPath.toFile())) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(dataArray, writer);
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
